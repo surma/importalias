c753b350098a4cfce3fb82a8e253fd7dfc10ea2f
Alexander Surma <surma@surmair.de>
2013-01-30 20:52:43 +0100

    Validate add alias form

--- static/js/validators.js
+++ static/js/validators.js
@@ -1,5 +1,6 @@
 define([], function() {
 	var DOMAIN_REGEXP = /^([a-z0-9-]+\.)+[a-z]{2,4}$/i
+	var PATH_REGEXP = /^(\/[^\/]+)+$/i
 	return {
 		'domain': function() {
 			return {
@@ -13,6 +14,24 @@ define([], function() {
 						} else {
 							console.log('invalid');
 							ctrl.$setValidity('domain', false);
+							return undefined;
+						}
+					});
+				}
+			};
+		},
+		'path': function() {
+			return {
+				require: 'ngModel',
+				link: function(scope, elm, attrs, ctrl) {
+					ctrl.$parsers.unshift(function(viewValue) {
+						if (PATH_REGEXP.test(viewValue)) {
+							console.log('valid');
+							ctrl.$setValidity('path', true);
+							return viewValue;
+						} else {
+							console.log('invalid');
+							ctrl.$setValidity('path', false);
 							return undefined;
 						}
 					});
